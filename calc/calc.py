e46b649b862ac6cd4f5137575909a1d2877788a9
trgordonb <trgordonb@gmail.com>
2019-01-09 00:09:18 +0800

    finish chapter 2

--- calc/calc.py
+++ calc/calc.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+
+class Calc:
+    def add(self, *args):
+        return sum(args)
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, *args):
+        if not all(args):
+            raise ValueError
+        return reduce(lambda x, y: x*y , args)
+
+    def div(self, a, b):
+        if not b:
+            return "inf"
+
+        return a/b
+
+    def avg(self, it, lt=None, ut=None):
+        _it = it[:]
+            
+        if lt:
+            _it = [x for x in _it if x >= lt]
+
+        if ut:
+            _it = [x for x in _it if x <= ut]
+
+        if not len(_it):
+            return 0
+
+        return sum(_it)/len(_it)
